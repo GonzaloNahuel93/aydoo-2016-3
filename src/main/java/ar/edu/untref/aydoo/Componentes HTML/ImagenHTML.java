9cff15f65015b66e654b0086d3991575dd47c87b
Gonzalo Nahuel <Gonzalo Nahuel>
2016-05-20 13:26:36 -0300

    Se edita ImagenHTML.java para verificar que se esté corriendo jenkins

--- src/main/java/ar/edu/untref/aydoo/Componentes HTML/ImagenHTML.java
+++ src/main/java/ar/edu/untref/aydoo/Componentes HTML/ImagenHTML.java
@@ -3,7 +3,7 @@ package D.ComponentesHTML;
 public class ImagenHTML extends LineaHTML{
 
 	public ImagenHTML(String contenido){
-		super("<img src=\"" + contenido + "\"/>");
+		super("<img src=\"" + contenido + "\"/>")
 	}
 
 }
