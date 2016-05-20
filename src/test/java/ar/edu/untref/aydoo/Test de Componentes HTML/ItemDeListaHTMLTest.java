1f21570991aa895249b25d433e54a3f1be4477b5
Gonzalo Nahuel <Gonzalo Nahuel>
2016-05-20 08:40:18 -0300

    Se agrega Test de la clase ItemDeListaHTML

---
+++ src/test/java/ar/edu/untref/aydoo/Test de Componentes HTML/ItemDeListaHTMLTest.java
@@ -0,0 +1,18 @@
+package D.ComponentesHTML;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ItemDeListaHTMLTest {
+
+	@Test
+	public void seColocaUnContenidoDeUnItemDeListaParaPasarloAItemDeListaEnHTML(){
+
+		ItemDeListaHTML itemDeListaHTML = new ItemDeListaHTML("Esto es un Item de una Lista");
+		String contenidoDelItemDeListaHTML = itemDeListaHTML.getContenido();
+
+		Assert.assertTrue(contenidoDelItemDeListaHTML.equals("<li>Esto es un Item de una Lista</li>"));
+
+	}
+
+}
