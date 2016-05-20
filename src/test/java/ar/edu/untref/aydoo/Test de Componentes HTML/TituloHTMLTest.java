73be0c89c9d12964aac22ffc658aaf93cbf1e014
Gonzalo Nahuel <Gonzalo Nahuel>
2016-05-20 08:29:55 -0300

    Agregado el Test de la clase TituloHTML

---
+++ src/test/java/ar/edu/untref/aydoo/Test de Componentes HTML/TituloHTMLTest.java
@@ -0,0 +1,18 @@
+package D.ComponentesHTML;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TituloHTMLTest {
+
+	@Test
+	public void seColocaUnContenidoDeUnTituloParaPasarloATituloEnHTML(){
+
+		TituloHTML tituloHTML = new TituloHTML("Esto es un Titulo");
+		String contenidoDelTituloHTML = tituloHTML.getContenido();
+
+		Assert.assertTrue(contenidoDelTituloHTML.equals("<h1>Esto es un Titulo</h1>"));
+
+	}
+
+}
