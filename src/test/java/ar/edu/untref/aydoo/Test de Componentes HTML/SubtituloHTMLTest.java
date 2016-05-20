7616ea36bdf39a6334010c18b70f72fda476a432
Gonzalo Nahuel <Gonzalo Nahuel>
2016-05-20 08:33:04 -0300

    Agregada la clase de Test para SubtituloHTML

---
+++ src/test/java/ar/edu/untref/aydoo/Test de Componentes HTML/SubtituloHTMLTest.java
@@ -0,0 +1,18 @@
+package D.ComponentesHTML;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SubtituloHTMLTest {
+
+	@Test
+	public void seColocaUnContenidoDeUnSubtituloParaPasarloASubtituloEnHTML(){
+
+		SubtituloHTML subtituloHTML = new SubtituloHTML("Esto es un Subtitulo");
+		String contenidoDelSubtituloHTML = subtituloHTML.getContenido();
+
+		Assert.assertTrue(contenidoDelSubtituloHTML.equals("<h2>Esto es un Subtitulo</h2>"));
+
+	}
+
+}
