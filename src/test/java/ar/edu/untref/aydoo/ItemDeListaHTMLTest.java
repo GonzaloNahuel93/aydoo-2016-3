318d92c13a3693d9f7eef7f68b33e2b945ee1371
Gonzalo Nahuel <Gonzalo Nahuel>
2016-05-22 17:35:38 -0300

    Otro test al ItemDeListaHTML -> que se transforme correctamente a codigo html cuando hay un salto de linea

--- src/test/java/ar/edu/untref/aydoo/ItemDeListaHTMLTest.java
+++ src/test/java/ar/edu/untref/aydoo/ItemDeListaHTMLTest.java
@@ -17,4 +17,16 @@ public class ItemDeListaHTMLTest {
 
 	}
 
+	@Test
+	public void queLaLineaEnHTMLDeUnItemDeListaSeEscribaCorrectamenteCuandoHayUnSalto(){
+
+		ItemDeListaHTML itemDeListaHTML = new ItemDeListaHTML("Esto es un \n" + 
+															  "Item de una Lista");
+		String contenidoDelItemDeListaHTML = itemDeListaHTML.getContenido();
+
+		Assert.assertTrue(contenidoDelItemDeListaHTML.equals("<li>Esto es un \n" +
+															 "Item de una Lista</li>"));
+
+	}
+
 }
