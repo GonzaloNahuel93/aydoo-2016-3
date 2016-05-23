1d4a1e6f4854e2dd1429092885642ca4fe7454d7
Gonzalo Nahuel <Gonzalo Nahuel>
2016-05-23 18:13:01 -0300

    El validadorDeParametros pasa su segundo test --> Que se detecte una contradiccion cuando se pide que sea en mode no output pero que se especifique un output como nombre de archivo de salida. Esto determinara si se prosigue con la transformacion del archivo

--- src/main/java/ar/edu/untref/aydoo/ValidadorDeParametros.java
+++ src/main/java/ar/edu/untref/aydoo/ValidadorDeParametros.java
@@ -3,7 +3,40 @@ package ar.edu.untref.aydoo;
 public class ValidadorDeParametros {
 
 	public boolean hayCondicionesValidasParaTransformar(String[] parametrosDeEntrada){
-		return true;
+
+		boolean hayCondicionesValidasParaTransformar = !this.contradiccionDeParametros(parametrosDeEntrada[0], parametrosDeEntrada[2]) && 
+										 				this.hayArchivoDeEntrada(parametrosDeEntrada[1]);
+
+		return hayCondicionesValidasParaTransformar;
+
 	}
-	
+
+	private boolean contradiccionDeParametros(String primerParametro, String segundoParametro) {
+		
+		boolean contradiccionDeParametros = false;
+		
+		boolean sePidioModoNoOutput = primerParametro.equals("--mode=no-output");
+		boolean estaVacioElParametroParaPedirNombreAUnArchivoDeSalida = segundoParametro.equals("");
+		
+		if(sePidioModoNoOutput && !estaVacioElParametroParaPedirNombreAUnArchivoDeSalida){
+			contradiccionDeParametros = true;
+		}
+		
+		return contradiccionDeParametros;
+		
+	}
+
+	private boolean hayArchivoDeEntrada(String parametro) {
+
+		boolean hayArchivoDeEntrada = false;
+		boolean estaVacioElParametro = parametro.equals("");
+
+		if(!estaVacioElParametro){
+			hayArchivoDeEntrada = true;
+		}
+
+		return hayArchivoDeEntrada;
+
+	}
+
 }
--- src/test/java/ar/edu/untref/aydoo/ValidadorDeParametrosTest.java
+++ src/test/java/ar/edu/untref/aydoo/ValidadorDeParametrosTest.java
@@ -10,11 +10,20 @@ public class ValidadorDeParametrosTest {
 	public void queTireTrueCuandoLosParametrosSonCorrectos(){
 
 		String[] parametros = {"--mode=default", "Mi Presentacion.md", "--output=Presentacion para AyDOO"};
-
 		ValidadorDeParametros validador = new ValidadorDeParametros();
 
 		Assert.assertTrue(validador.hayCondicionesValidasParaTransformar(parametros));
 
 	}
-	
+
+	@Test
+	public void queTireFalseCuandoLosParametrosDelModeYOutputSeContradicen(){
+
+		String[] parametros = {"--mode=no-output", "miPresentacion.md", "--output=presentacionMaster"};
+		ValidadorDeParametros validador = new ValidadorDeParametros();
+
+		Assert.assertFalse(validador.hayCondicionesValidasParaTransformar(parametros));
+
+	}
+
 }
