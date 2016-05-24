6b54c1dbca13482d087a6caf91eb692f4e36c2f9
SynysterLove <synlgp@gmail.com>
2016-05-24 16:45:28 -0300

    Corrijo errores de escritura al copiar lso tests

--- src/test/java/ar/edu/untref/aydoo/ItemDeListaTest.java
+++ src/test/java/ar/edu/untref/aydoo/ItemDeListaTest.java
@@ -6,36 +6,36 @@ import org.junit.Test;
 /**
  * Created by synysterlove on 24/05/16.
  */
-public class ImagenTest {
-    
+public class ItemDeListaTest {
+
     @Test
-    public void queDosImagenesIgualesSeanIguales() {
-        
+    public void queDosSubtitulosIgualesSeanIguales() {
+
         Imagen imagen1 = new Imagen("Imagen.png");
         Imagen imagen2 = new Imagen("Imagen.png");
-        
+
         Assert.assertTrue(imagen1.equals(imagen2));
-        
+
     }
-    
+
     @Test
-    public void queDosImagenesDistintosSeanDistintos() {
-        
+    public void queDosSubtitulosDistintosSeanDistintos() {
+
         Imagen imagen1 = new Imagen("Imagen1.png");
         Imagen imagen2 = new Imagen("Imagen2.png");
-        
+
         Assert.assertFalse(imagen1.equals(imagen2));
-        
+
     }
-    
+
     @Test
-    public void queElContenidoDeLaImagenSeSeteeCorrectamenteAPartirDelStringQueLaRepresenta() {
-        
+    public void queElContenidoDelSubtituloSeSeteeCorrectamenteAPartirDelStringQueLoRepresenta() {
+
         Imagen imagen = new Imagen("");
         imagen.colocarStringDeContenidoPropio("i:imagen.png");
-        
+
         Assert.assertEquals("imagen.png", imagen.getContenido());
-        
+
     }
     
 }
