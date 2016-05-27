fa368af53d8f99179655dbaa2110e70b6b37e43b
SynysterLove <synlgp@gmail.com>
2016-05-26 23:10:02 -0300

    Agrego otro test al equals, corrijo algo y pasa

--- src/test/java/ar/edu/untref/aydoo/SeccionTest.java
+++ src/test/java/ar/edu/untref/aydoo/SeccionTest.java
@@ -35,7 +35,7 @@ public class SeccionTest {
 
 
     @Test
-    public void queDosListasConDistintosElementosSeanDistintas() {
+    public void queDosSeccionesConDistintosElementosSeanDistintas() {
 
         Seccion seccion1 = new Seccion();
         Seccion seccion2 = new Seccion();
@@ -56,6 +56,30 @@ public class SeccionTest {
         seccion2.agregarContenido(titulo);
         seccion2.agregarContenido(lista);
         seccion2.agregarContenido(subtitulo2);
+
+        Assert.assertNotEquals(seccion1, seccion2);
+
+    }
+
+    @Test
+    public void queDosSeccionesConDiferenteTamanioSeanDistintas() {
+
+        Seccion seccion1 = new Seccion();
+        Seccion seccion2 = new Seccion();
+
+        Titulo titulo = new Titulo("Titulo");
+        ItemDeLista item1 = new ItemDeLista("Item1");
+        ItemDeLista item2 = new ItemDeLista("Item2");
+        Lista lista = new Lista();
+        lista.agregarItem(item1);
+        lista.agregarItem(item2);
+        Subtitulo subtitulo = new Subtitulo("Subtitulo");
+
+        seccion1.agregarContenido(titulo);
+        seccion1.agregarContenido(lista);
+        seccion1.agregarContenido(subtitulo);
+        seccion2.agregarContenido(titulo);
+        seccion2.agregarContenido(lista);
 
         Assert.assertNotEquals(seccion1, seccion2);
 
